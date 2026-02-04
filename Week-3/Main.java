35948843538cc97aafb5ea1f6d2ba39089f5b829
nurcanyuruk-2023 <152140659+nurcanyuruk-2023@users.noreply.github.com>
2026-02-04 09:48:52 -0600

    Update Main.java

--- Week-3/Main.java
+++ Week-3/Main.java
@@ -2,8 +2,7 @@ import edu.princeton.cs.algs4.StdIn;
 
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+       
         StackofStrings stack = new StackofStrings();
 
         while(!StdIn.isEmpty()) {
